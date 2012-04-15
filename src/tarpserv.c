101c09411640c8b384bef9996f8d673e2995ce51
Abhishek Shroff <me@shroffabhishek.net>
2012-04-15 00:28:25 -0400

    Makefile modifications

--- src/tarpserv.c
+++ src/tarpserv.c
@@ -1,13 +1,21 @@
+typedef unsigned char u_char;
+typedef unsigned short u_short;
+typedef unsigned int u_int;
+
 #include <stdio.h>
 #include <pcap.h>
 
 #define MAX_CAP_SIZE 2048
 
-pcap_t* tarpserv_open_pcap(char*);
+
+/* Function Prototypes */
+pcap_t* tarpserv_open_pcap(char*, char*);
+void dhcp_handler(u_char*, const struct pcap_pkthdr*, const u_char*);
 
 int main() {
-  //TODO: Do not compile fixed device name
+  /* TODO: Do not compile fixed device name */
   pcap_t* dhcp_session = tarpserv_open_pcap("eth0", "udp and port 67");
+  pcap_loop(dhcp_session, -1, dhcp_handler, NULL);
   return 0;
 }
 
@@ -40,3 +48,8 @@ pcap_t* tarpserv_open_pcap(char* dev, char* filter) {
 
   return pcap_session;
 } 
+
+void dhcp_handler(u_char *args,
+    const struct pcap_pkthdr *header,
+    const u_char *packet) {
+}
